1b8bf05796174d9961764e2b2d75452c91bb13fb
478959472 <478959472@qq.com>
2020-06-28 23:18:21 +0800

    字幕文件名修改

---
+++ copy_data/change_file_name.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*
+import os
+
+
+
+dirName = 'F:/学习\人工智能/4、Visual Recognition & Understanding'
+listOfFiles = list()
+for (dirpath, dirnames, filenames) in os.walk(dirName):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+for elem in listOfFiles:
+    if elem.endswith(".srt") and "[baidu-en-zh]" in elem:
+        file_dir, file_name = os.path.split(elem)
+        print(file_name)
+        file_name_new = file_name.replace(".en", "")
+        file_name_new = file_name_new.replace("[baidu-en-zh] ", "")
+        print(elem)
+        print(os.path.join(file_dir, file_name_new))
+        os.rename(elem, os.path.join(file_dir, file_name_new))
