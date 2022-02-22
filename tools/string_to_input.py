301e2933aa2ced412cfe71774c5e8af96bb39d63
cc <478959472@qq.com>
2022-02-22 16:45:14 +0800

    字符串转键盘

---
+++ tools/string_to_input.py
@@ -0,0 +1,19 @@
+import keyboard
+import time
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+clipboard = app.clipboard()
+
+data = clipboard.mimeData()
+
+if('text/plain' in data.formats()):
+    data = data.text()
+    print(data)
+    print("睡眠3秒")
+    time.sleep(3)
+    keyboard.write(data)
+    # for c in data:
+    #     #do something with c
+    #     time.sleep(0.1)
+    #     keyboard.press_and_release(c)
