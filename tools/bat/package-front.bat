73b673b0da8cb69bce8f6e691f54f66ff9d2bfa2
cc <478959472@qq.com>
2022-08-05 15:18:11 +0800

    自动化打包前端bat

---
+++ tools/bat/package-front.bat
@@ -0,0 +1,39 @@
+:: 目标路径 C:\work\montnets\twise-packges
+:: 落地页
+cd C:\work\montnets\5g-ccc-twise\web-landing-page
+call npm install
+call npm run build
+7z a twise-5g-ccc-landing-page-front.zip ./dist
+echo "twise-5g-ccc-landing-page-front.zip"
+:: 复制到 C:\work\montnets\twise-packges
+xcopy twise-5g-ccc-landing-page-front.zip C:\work\montnets\twise-packges\ /y
+
+:: 5g-ccc
+cd C:\work\montnets\5g-ccc-twise\web
+call npm install
+call npm run build
+7z a twise-5g-ccc-front.zip ./dist
+echo "twise-5g-ccc-front.zip"
+:: 复制到 C:\work\montnets\twise-packges
+xcopy twise-5g-ccc-front.zip C:\work\montnets\twise-packges\ /y
+
+
+:: 5g-aim
+cd C:\work\montnets\5g-aim\web-ec
+call npm install
+call npm run build
+7z a 5g-aim-ec-front.zip ./dist
+echo "5g-aim-ec-front.zip"
+:: 复制到 C:\work\montnets\twise-packges
+xcopy 5g-aim-ec-front.zip C:\work\montnets\twise-packges\ /y
+
+
+:: 5g-aim-editor
+cd C:\work\montnets\5g-aim\web-editor
+call npm install
+call npm run build
+7z a 5g-aim-editor-front.zip ./dist
+echo "5g-aim-ec-front.zip"
+:: 复制到 C:\work\montnets\twise-packges
+xcopy 5g-aim-editor-front.zip C:\work\montnets\twise-packges\ /y
+
