9fe5a423745cec7a2542007f191d0c81c1365d2e
cc <478959472@qq.com>
2022-09-28 11:24:03 +0800

    鼠标拖动

---
+++ tools/mouse_drag_to.py
@@ -0,0 +1,10 @@
+import time
+import pyautogui
+
+time.sleep(3)
+
+pyautogui.moveTo(620, 242)
+pyautogui.mouseDown(button='left')
+pyautogui.moveTo(961, 388, 1)
+pyautogui.mouseUp(button='left')
+time.sleep(5)
